05ff031b765eaa6c4f6ae0ecaa0bdc69078a206d
Anil239009 <anilbemech2020@gmail.com>
2023-11-30 23:33:54 +0530

    tree:

--- Tree_DS/src/Code/Test.java
+++ Tree_DS/src/Code/Test.java
@@ -3,30 +3,42 @@ package Code;
 public class Test {
 
 	public static void main(String[] args) {
-		BTS bts=new BTS();
-		bts.insert(50);
-		bts.insert(10);
-		bts.insert(80);
-		bts.insert(20);
-		bts.insert(30);
-		bts.insert(60);
+		BTS bst=new BTS();
+		bst.insert(50);
+		bst.insert(10);
+		bst.insert(80);
+		bst.insert(20);
+		bst.insert(30);
+		bst.insert(60);
 		
 		System.out.println("preOrder : ");
-		bts.preOrder();
+		bst.preOrder();
 		System.out.println();
 		System.out.print("PreOrder : ");
-		bts.preOrder(bts.root);
+		bst.preOrder(bst.root);
 		System.out.println();
 		System.out.print("InOrder : ");
-		bts.inOrder(bts.root);
+		bst.inOrder(bst.root);
 		System.out.println();
 		System.out.print("PostOrder : ");
-		bts.postOrder(bts.root);
+		bst.postOrder(bst.root);
 		System.out.println();
+
 		
-		int count=bts.getcount(bts.root);
+		int count=bst.getcount(bst.root);
 		System.out.println("Count of element is : "+count);
 		
+		System.out.println("60? " + bst.find(bst.root, 60));
+        System.out.println("70? " + bst.find(bst.root,70));
+        System.out.println("50? " + bst.find(bst.root, 50));
+        System.out.println("160? " + bst.find(bst.root, 160));
+
+        bst.delete(50);
+        bst.preOrder();
+        
+
+        System.out.println("Del 49: " + bst.delete(49));
+
 		
 	}
 
